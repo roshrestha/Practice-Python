faaaa008f34b5c2333e0c9229717fcd4137594e4
Roshan Shrestha <roshan_pra@hotmail.com>
2020-06-06 23:45:26 +0545

    added some codes

---
+++ PythonOOPLinkedin/basic_class_def.py
@@ -0,0 +1,10 @@
+class Book:
+    def __init__(self, title):
+        self.title = title
+
+b1 = Book("Brave New World") #Creating object or instance of the class by calling in the class name Book as a constructor function
+ 
+b2 = Book("War and Peace")
+
+print(b1)
+print(b1.title)
